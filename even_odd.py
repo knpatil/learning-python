7f40a1aa14d1418f0c2f98ec93ef4a25c61e11fa
kpatil <kpatil@salesforce.com>
2020-06-10 21:08:26 -0700

    adding even_odd.py file

---
+++ even_odd.py
@@ -0,0 +1,25 @@
+
+
+# to add two numbers
+# comment
+
+# write a program to check if given number is even or odd
+
+# indentation
+def isEvenOdd(number):
+    if number % 2 == 0:  # % -- modulo operator -- remainder
+        print(number, " is Even")
+    else:
+        print(number, " is odd")
+
+isEvenOdd(142113) # execute this code
+
+# assignment
+
+
+
+    
+
+        
+    
+    
